4edde9757d429257413ad004d1912a877739646c
Erkan Sen <erkansen14@hotmail.com>
2016-10-03 19:48:38 -0500

    added blue gem, script, prefabs

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool sameType(Gem other) {
+		return GetComponent<SpriteRenderer> ().sprite == other.GetComponent<SpriteRenderer> ().sprite;
+	}
+}
