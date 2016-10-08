5f9e7e9444f55eaaa4418df096d943d40dd4920e
Erkan Sen <erkansen14@hotmail.com>
2016-10-08 02:07:38 -0500

    added gravity reversal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,16 @@ public class Player : MonoBehaviour {
 	public float speed = 30;
 	// Update is called once per frame
 	void Update () {
+		//Move Horizontal
 		float h = Input.GetAxisRaw ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * h * speed;
+
+		//Gravity Change
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().gravityScale *= -1;
+			transform.Rotate (0, 180, 180);
+		}
+		// Animation
+		GetComponent<Animator>().SetInteger("DirX", (int)h);
 	}
 }
