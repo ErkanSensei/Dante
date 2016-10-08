8bf9220a8927f81a93946cd1649826a8a55c4150
Erkan Sen <erkansen14@hotmail.com>
2016-10-08 01:58:16 -0500

    added player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 30;
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+	}
+}
