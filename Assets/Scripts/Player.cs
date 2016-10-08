02748fe0585bda394fdbc7c75a15fe5e6d43da8c
Erkan Sen <erkansen14@hotmail.com>
2016-10-08 02:12:50 -0500

    added chek for character grounded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,27 @@ public class Player : MonoBehaviour {
 		GetComponent<Rigidbody2D> ().velocity = Vector2.right * h * speed;
 
 		//Gravity Change
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded()) {
 			GetComponent<Rigidbody2D> ().gravityScale *= -1;
 			transform.Rotate (0, 180, 180);
 		}
 		// Animation
 		GetComponent<Animator>().SetInteger("DirX", (int)h);
+	}
+
+	public bool isGrounded() {
+		// Get Bounds and Cast Range (extents.y+20%)
+		Bounds bounds = GetComponent<Collider2D>().bounds;
+		float range = bounds.extents.y * 1.2f;
+
+		// Calculate a position slightly 'below' the collider
+		// (via transform.up so it's gravity tolerant)
+		Vector2 v = bounds.center - transform.up * range;
+
+		// Linecast upwards
+		RaycastHit2D hit = Physics2D.Linecast(v, bounds.center);
+
+		// Was there something in-between, or did we hit ourself?
+		return (hit.collider.gameObject != gameObject);
 	}
 }
