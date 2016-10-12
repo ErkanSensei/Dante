5e764e0c279860b22cdd77846310d986ef650364
Ahmed <ahmedgouda9484@gmail.com>
2016-10-12 17:53:18 -0500

    Added ball and ball's upward movement.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 100.0f;  //speed of ball
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.up * speed;
+	}
+}
