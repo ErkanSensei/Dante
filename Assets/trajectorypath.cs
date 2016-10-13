f5cd806c3b965f0d4bff24870b8c83d129c60ab0
Keith Russell <keithrmathman@yahoo.com>
2016-10-13 02:14:50 -0500

    Puzzle 1 Assets for game
    Objects I designed for puzzle 1

---
+++ Assets/trajectorypath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trajectorypath : MonoBehaviour {
+	
+		private GameObject[] locationsToSpawn;
+		private float counter = 0;
+		[SerializeField] string[] listOfPossibleTags;
+		[SerializeField] GameObject[] objectToSpawn;
+		[SerializeField] float timeBetweenSpawns = 3.0f;
+
+		void  Start (){
+			locationsToSpawn = GameObject.FindGameObjectsWithTag("SpawnLocation");
+		}
+		void  Update (){
+			counter+= Time.deltaTime;
+			if(counter > timeBetweenSpawns){
+				GameObject spawnedObject;
+				spawnedObject = Instantiate(objectToSpawn[Random.Range(0, objectToSpawn.Length)], locationsToSpawn[Random.Range(0, locationsToSpawn.Length)].transform.position, Quaternion.identity) as GameObject;
+				spawnedObject.gameObject.tag = listOfPossibleTags[Random.Range(0, listOfPossibleTags.Length)];
+				counter = 0;
+			}
+		}
+	}
+
