12d55927ea9944675ab354407b5775ca6921fad8
Erkan Sen <erkansen14@hotmail.com>
2016-10-20 14:43:41 -0500

    puzzle spawn trigger works

---
+++ Assets/Scripts/TriggerBejeweled.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBejeweled : MonoBehaviour {
+	public GameObject BJewel;
+	bool entered = false;
+
+	void onTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Triggered");
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (!entered) {
+			Debug.Log ("TriggeredStay");
+			BJewel.SetActive (true);
+			entered = true;
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
