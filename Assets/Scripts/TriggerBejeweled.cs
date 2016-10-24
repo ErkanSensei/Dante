f689902366ec9c574b1677a54427ba4591d17679
Erkan Sen <erkansen14@hotmail.com>
2016-10-24 02:03:11 -0500

    mini games trigger properly

--- Assets/Scripts/TriggerBejeweled.cs
+++ Assets/Scripts/TriggerBejeweled.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class TriggerBejeweled : MonoBehaviour {
 	public GameObject BJewel;
+	public Player player;
 	public GameObject[] gos;
+	public GameObject[] controls;
+
 	bool entered = false;
 
 	void onTriggerEnter2D(Collider2D other) {
@@ -14,10 +17,18 @@ public class TriggerBejeweled : MonoBehaviour {
 	{
 		if (!entered) {
 			gos = GameObject.FindGameObjectsWithTag("Puzzle");
+			controls = GameObject.FindGameObjectsWithTag ("Control");
 			//loop through the returned array of game objects and set each to active false
 			foreach (GameObject go in gos)  {
 				go.SetActive(false);
 				Destroy (go);
+			}
+			foreach (GameObject control in controls)  {
+				player.StopMeUp ();
+				player.StopMeDown ();
+				player.StopMeLeft ();
+				player.StopMeRight ();
+				control.SetActive(false);
 			}
 			BJewel.SetActive (true);
 			entered = true;
