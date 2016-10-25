4cb9d811ddc98d0586a66aadda68aceab65061f6
Ahmed <ahmedgouda9484@gmail.com>
2016-10-24 19:26:19 -0500

    added touch controls?????

--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -7,8 +7,20 @@ public class Racket : MonoBehaviour {
 
 	// Use this for initialization
 	void FixedUpdate(){
-	
-		float yInput = Input.GetAxisRaw ("Horizontal");	// Horizontal input.
-		GetComponent<Rigidbody2D>().velocity = Vector2.right * yInput * speed;
+
+		// Moves object according to finger movement on the screen
+		if (Input.touchCount > 0 &&
+		     Input.GetTouch (0).phase == TouchPhase.Moved) {
+
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+
+			// Move object across XY plane
+			transform.Translate (-touchDeltaPosition.x * speed, 
+				-touchDeltaPosition.y * speed, 0);
+		}
 	}
+		//float yInput = Input.GetAxisRaw ("Horizontal");	// Horizontal input.
+		//GetComponent<Rigidbody2D>().velocity = Vector2.right * pointer_y * speed
 }
+
