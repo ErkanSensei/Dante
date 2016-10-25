d6a8990d801b571bd63e187adfceafe06a06c948
Erkan Sen <erkansen14@hotmail.com>
2016-10-24 19:25:57 -0500

    touch rotation control stuff, holy shit lots of code

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hexagon : MonoBehaviour {
+	public GameObject[] rotations;
+	public bool[] currentRotations;
+	public double currentRotation;
+	public double[] winningRotations;
+	public double winningRotation;
+	public bool rotateOrNo = false;
+	
+	// Update is called once per frame
+	void Update () {
+		checkIfWinning ();
+	}
+
+	public void rotateThis(GameObject hexagon) {
+		Debug.Log (Mathf.Floor(hexagon.transform.localEulerAngles.z));
+		hexagon.transform.Rotate (0, 0, 60);
+	}
+
+	public void checkIfWinning() {
+		for (int i = 0; i < rotations.Length; i++) {
+			if (winningRotations [i] == Mathf.Floor (rotations [i].transform.localEulerAngles.z)) {
+				currentRotations [i] = true;
+			} else {
+				currentRotations [i] = false;
+			}
+		}
+	}
+}
