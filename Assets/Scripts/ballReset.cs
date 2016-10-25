1591c273cd4e795c4b52120f7f1989db7cc595b6
Ahmed <ahmedgouda9484@gmail.com>
2016-10-25 13:40:49 -0500

    Added level.

---
+++ Assets/Scripts/ballReset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballReset : MonoBehaviour {
+	public Vector3 initialPosition;
+	Ball instance = new Ball();
+	void OnTriggerEnter(Collider go){
+		// Add some checks to make sure the go (gameobject) is the ball
+		transform.position = initialPosition;
+	}
+}
